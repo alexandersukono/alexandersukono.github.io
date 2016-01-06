7249caccd3a18773baa9741dda2fcb03b900df66
alex <alexander.sukono@gmail.com>
2016-01-06 16:23:56 +0700

    update for SEO

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -6,3 +6,17 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function closeSignIn() {
+			$('#signin-modal').closeModal();
+			setTimeout(function(){
+				$('#signup-modal').openModal();
+			}, 300);
+}
+		
+  function closeSignUp() {
+			$('#signup-modal').closeModal();
+			setTimeout(function(){
+				$('#signin-modal').openModal();
+			}, 300)
+}
